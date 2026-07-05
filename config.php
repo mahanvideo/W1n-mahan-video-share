aa4711e0ac74d3896c1b483e30c59851bc87739b
mahanvideo <aparatnull@gmail.com>
2026-07-05 09:14:14 +0330

    Add config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "w1n_mahan";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL)");
+$conn->query("CREATE TABLE IF NOT EXISTS videos (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, path VARCHAR(255) NOT NULL, user VARCHAR(50) NOT NULL, upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)");
+?>
